9ff9e5deb2822064e86ec5847527fd7f3215e167
theodorciuciuc <ciuciuc.teodor@gmail.com>
2022-04-11 12:24:27 +0300

    task A

---
+++ PracticalTest01Var07/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest01var07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest01Var07MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var07_main);
+    }
+}
